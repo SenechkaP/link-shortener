e7a7b2eee946a047ff5fbc9358ca0c095a3a365b
Арсений <arsenypadaltsyn@yandex.ru>
2025-07-30 13:28:37 +0300

    service register test

---
+++ internal/auth/service_test.go
@@ -0,0 +1,28 @@
+package auth
+
+import (
+	"advpractice/internal/user"
+	"testing"
+)
+
+type MockUserRepository struct{}
+
+func (*MockUserRepository) Create(*user.User) (*user.User, error) {
+	return &user.User{}, nil
+}
+
+func (*MockUserRepository) GetByEmail(string) (*user.User, error) {
+	return nil, nil
+}
+
+func TestRegisterSuccess(t *testing.T) {
+	authService := NewAuthService(&MockUserRepository{})
+	err := authService.register(&RegistrateRequest{
+		Name:     "test",
+		Email:    "test@test.test",
+		Password: "testpassword",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
